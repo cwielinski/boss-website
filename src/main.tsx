3d870ee0f40483f636abeadbc7ee012f850917aa
cwielinski <chris@thinkcre8tive.com>
2026-01-02 11:37:56 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 
 // ✅ Make sure these are NOT commented out
-import "./app/styles/tailwind.css";
-import "./app/styles/theme.css";
-import "./app/styles/fonts.css";
-import "./app/styles/index.css";
+import "./src/styles/tailwind.css";
+import "./src/styles/theme.css";
+import "./src/styles/fonts.css";
+import "./src/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
